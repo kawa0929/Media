87ab2be702d46abdff71445c77d4f8d7c3a6e034
student <melisaqq55@gmail.com>
2024-10-28 16:58:26 +0800

    3

--- app/src/main/java/com/example/media/MainActivity.kt
+++ app/src/main/java/com/example/media/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.media
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Button
@@ -16,8 +18,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.media.ui.theme.MediaTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,40 +43,59 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    var Animals = arrayListOf(R.drawable.animal0, R.drawable.animal1,
+    var Animals = arrayListOf(
+        R.drawable.animal0, R.drawable.animal1,
         R.drawable.animal2, R.drawable.animal3,
         R.drawable.animal4, R.drawable.animal5,
         R.drawable.animal6, R.drawable.animal7,
-        R.drawable.animal8, R.drawable.animal9)
-    var AnimalsName = arrayListOf("鴨子","企鵝","青蛙","貓頭鷹","海豚", "牛", "無尾熊", "獅子", "狐狸", "小雞")
+        R.drawable.animal8, R.drawable.animal9
+    )
+    var AnimalsName = arrayListOf(
+        "鴨子", "企鵝", "青蛙", "貓頭鷹", "海豚", "牛", "無尾熊", "獅子", "狐狸", "小雞"
+    )
+    val context = LocalContext.current
+    var mper = MediaPlayer()
 
     Column {
 
         Row {
             Button(onClick = {
-                //your onclick code here
+                mper.reset()
+                mper = MediaPlayer.create(context, R.raw.tcyang)
+                mper.start()
             }) {
+
                 Text(text = "歡迎修課")
+                Image(
+                    painter = painterResource(id = R.drawable.teacher),
+                    contentDescription = "老師",
+                    Modifier.size(80.dp)
+                )
+
             }
 
             Button(onClick = {
-                //your onclick code here
+                mper.reset()
+                mper = MediaPlayer.create(context, R.raw.fly)
+                mper.start()
             }) {
+
                 Text(text = "展翅飛翔")
             }
         }
-            LazyRow {
+        LazyRow {
             items(50) { index ->
                 Text(index.toString(), modifier = modifier)
-                Text(text = AnimalsName[index % 10],modifier = modifier)
-                    Image(
-                        painter = painterResource(id = Animals[index % 10]),
-                        contentDescription = "動物0",
-                        modifier = Modifier.fillParentMaxWidth(1.0f))
+                Text(text = AnimalsName[index % 10], modifier = modifier)
+                Image(
+                    painter = painterResource(id = Animals[index % 10]),
+                    contentDescription = "動物0",
+                    modifier = Modifier.fillParentMaxWidth(1.0f)
+                )
 
 
-                }
             }
+        }
 
         LazyColumn {
 
